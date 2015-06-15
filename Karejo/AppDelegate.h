0c5288163af6a11c278e9f27fe598be748d83787
Arie Prasetyo <aprasetyo@traveloka.com>
2015-06-15 14:01:43 +0700

    Initial Commit

---
+++ Karejo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Karejo
+//
+//  Created by Arie Prasetyo on 6/15/15.
+//  Copyright (c) 2015 Arie Prasetyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Karejo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Karejo
+//
+//  Created by Arie Prasetyo on 6/15/15.
+//  Copyright (c) 2015 Arie Prasetyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
